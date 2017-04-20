7978f3604e08b01e9d86995d7bf05269ccb4be44
Shaojung Lu <shaojung@gmail.com>
2017-04-20 13:46:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yvtc/yvd042003/Student.java
@@ -0,0 +1,11 @@
+package com.example.yvtc.yvd042003;
+
+/**
+ * Created by yvtc on 2017/4/20.
+ */
+
+public class Student {
+    public String Name;
+    public String Addr;
+    public String Tel;
+}
